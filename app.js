b9b45609523f6b2a7e624c71c0f5d64461df7bf9
ageru666 <ageru2003@gmail.com>
2024-04-15 22:10:58 +0300

    ii

--- app.js
+++ app.js
@@ -104,8 +104,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+app.get('/soups', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'soups.html'));
 });
+
 
 app.get('/drinks/:category', async (req, res) => {
   const { category } = req.params;
