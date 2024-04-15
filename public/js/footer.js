ca857cda6d7360a21a4da41218283ca8986d8418
ageru666 <ageru2003@gmail.com>
2024-04-15 23:52:33 +0300

    Update footer.js

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footerHTML = `
-        <footer class="text-white bg-dark">
+        <footer class="text-white bg-dark " style="bottom: 0;">
             <div class="container py-1">
                 <div class="row">
                     <div class="col-md-4">
