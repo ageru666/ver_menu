600e8c87b2d7ec0742d7914bf63a67ff033da313
ageru666 <ageru2003@gmail.com>
2024-04-15 22:37:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
 app.get('/soups', (req, res) => {
-  require(path.join(__dirname, 'public', 'js', 'header.js'));
+  require('header.js');
   res.sendFile(path.join(__dirname, 'public', 'html', 'soups.html'));
 });
 
