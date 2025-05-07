82147bb3c4c28b9b4486d6e3c7be71481e8db9b2
ageru666 <ageru2003@gmail.com>
2025-05-07 17:52:45 +0300

    Fetch changes

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -140,7 +140,7 @@ const CheckoutPage = () => {
         headers['Authorization'] = `Bearer ${token}`;
       }
 
-      const response = await fetch('http://localhost:3002/api/orders/create', {
+      const response = await fetch('${process.env.REACT_APP_API_URL}/api/orders/create', {
         method: 'POST',
         headers,
         body: JSON.stringify(orderData),
