8e2b2a3c2f65bf229f833eb427ad16db9fd1fdea
ageru666 <ageru2003@gmail.com>
2025-05-07 17:09:17 +0300

    Some changes

--- server/app.js
+++ server/app.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
